7cbfbc447542c5fb0bab6c191aa41e9ead511f17
Vincent74230 <vincent.nowak@hotmail.fr>
2021-09-05 11:46:50 +0200

    ADDED pagination system, very simple: to be improved

--- grababeer/home/views.py
+++ grababeer/home/views.py
@@ -3,21 +3,32 @@ from django.http import HttpResponse
 from .grababeer_extends import beers, single_beer
  
 def index(request):
-    page=1
 
-    page = request.GET.get('page')
-
-    if not page:
+    pagination = request.GET.get('pagination')
+    if not pagination:
         page = 1
+        list_of_beers = beers(page)
+        context = {
+        'page':page,
+        'list_of_beers':list_of_beers,
+        }
+        return render(request, 'home/index.html', context)
+    else:
+        page = request.GET.get('page')
+        page = int(page)
+        pagination = request.GET.get('pagination')
+        if pagination == 'previous':
+            page -= 1
+        else:
+            page += 1
 
-    list_of_beers = beers(page)
+        list_of_beers = beers(page)
 
-    context = {
-    'page':page,
-    'list_of_beers':list_of_beers,
-    }
-
-    return render(request, 'home/index.html', context)
+        context = {
+        'page':page,
+        'list_of_beers':list_of_beers,
+        }
+        return render(request, 'home/index.html', context)
 
 def save_favourite(request):
     beer = request.GET.get('favourite_beer')
